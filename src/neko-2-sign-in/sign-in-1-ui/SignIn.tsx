d655ee908575f1352917a91209841e044d0ff4f4
Bbara <backdown959@gmail.com>
2019-12-13 20:00:40 +0300

    add jsx

--- src/neko-2-sign-in/sign-in-1-ui/SignIn.tsx
+++ src/neko-2-sign-in/sign-in-1-ui/SignIn.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import s from './SignIn.module.css'
+import {FORGOT_PATH, REGISTER_PATH} from "../../neko-1-main/main-1-ui/Routes";
+import {NavLink} from "react-router-dom";
 
 interface SignInProps {
 
@@ -7,8 +10,13 @@ interface SignInProps {
 const SignIn: React.FC<SignInProps> = ({}) => {
 
     return (
-        <div>
-            sign-in
+        <div className={s.content}>
+            <input placeholder='login'/>
+            <input placeholder='password'/>
+            <NavLink to={FORGOT_PATH}>Forgot password?</NavLink>
+            <button>Sign in</button>
+            <input type='checkbox'/> Remember me
+            <NavLink to={REGISTER_PATH}>Registartion</NavLink>
         </div>
     );
 };
